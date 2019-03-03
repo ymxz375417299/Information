da0ad97848a0237411d58dd26a0f754d1053a83a
rock <ymxz34787409@126.com>
2019-03-04 03:02:09 +0800

    抽离代码_config部分

---
+++ config.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# 配置模块
+
+import redis
+
+
+class Config:
+    """工程配置信息"""
+    DEBUG = True
+    # # 默认host和端口
+    # HOST = '0.0.0.0'
+    # PORT = 5000
+    # mysql配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    # 安全秘钥
+    SECRET_KEY = 'RFIAGD3GEemj+qx7oUa2IERSABk9xhHpnmGse6FGtiA='
+    SESSION_TYPE = 'redis' # session保存到redis
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 使用自己导入的redis的实例，默认会自己帮我们传，但是会把端口配置固定，不够灵活
+    SESSION_USE_SIGNER = True # 让cookie中的session_id 被加密签名处理
+    SESSION_PERMANENT = False # 设置session过期
+    PERMANENT_SESSION_LIFETIME = 86400 * 2 # session的有效期2天，单位秒，默认31天
