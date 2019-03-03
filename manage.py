103f8908c206bb98fab3f92a7787f9d62d61591d
rock <ymxz34787409@126.com>
2019-03-03 22:54:49 +0800

    配置mysql,redis和一些基本配置

--- manage.py
+++ manage.py
@@ -1,10 +1,31 @@
+# coding=utf-8
+# 程序入口
+
+import redis
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config:
+    """工程配置信息"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return 'hello world'
+
 
 if __name__ == '__main__':
     app.run()
