4dace6a593b98eb8e0f7e36f5c28c743bfd5117d
rock <ymxz34787409@126.com>
2019-03-04 00:49:55 +0800

    添加flask_script 和 flask_megrate 配置

--- manage.py
+++ manage.py
@@ -7,12 +7,17 @@ from flask import Flask, session
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
 class Config:
     """工程配置信息"""
     DEBUG = True
+    # # 默认host和端口
+    # HOST = '0.0.0.0'
+    # PORT = 5000
     # mysql配置
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -29,12 +34,19 @@ class Config:
 
 
 
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 设置session保存位置，配置信息由app的config中提取，所以在config中设置session
 Session(app)
+# 迁移脚本初始化，讲app和db绑定
+Migrate(app, db)
+# flask脚本和app绑定
+manager = Manager(app)
+# 把迁移命令添加到manager脚本中
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/', methods=['GET'])
@@ -44,4 +56,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    manager.run()
