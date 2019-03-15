3d4be7bbfd586317628eed032eeb12573121ceaf
rock <ymxz34787409@126.com>
2019-03-15 18:32:50 +0800

    2.1.9 首页右上角内容逻辑显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -3,15 +3,33 @@
 
 from . import index_blu
 from info import redis_store
+from info.models import User
 from flask import current_app, render_template
+from flask import session
 
 
 @index_blu.route('/', methods=['GET'])
 def index():
-    """首页"""
-    current_app.logger.debug('logging testing!')
-    redis_store.set('name', 1111111)
-    return render_template('news/index.html')
+    """
+    首页
+    1. 如果用户已经登录，将当前登录用户的数据传到模板，供模板显示
+    """
+    # 获取当前登录的用户id(来自session)默认值为空
+    user_id = session.get('user_id', '')
+    # 通过id获取用户信息
+    user = None
+    if user_id:
+        # 尝试查询用户的模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    data = {
+        "user_info": user.to_dict() if user else None # 有值取值
+        # to_dict是flask的user模型的提供一个方法，把属性转成字典
+            }
+            
+    return render_template('news/index.html', data=data)
 
 
 @index_blu.route('/favicon.ico')
